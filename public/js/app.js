551ccb85d5ead9b8a9f4c770359812ee09f90234
aymen abbad andaloussi <aymenandaloussi69@gmail.com>
2025-05-12 22:58:32 +0100

    finish sign in and sign up and edit som false

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,16 @@
+class database {
+    constructor(name, email, age, password) {
+        this.name = name
+        this.email = email
+        this.age = age
+        this.password = password
+    }
+}
+let user = new database([], [], [], []);
+
 let exit = "exit"
 
-let enter = prompt("Write a number to choose between : \n1) sign up \n2) log in \n3) change the password").toLowerCase() // ila bghiti tkhorji ketbi exit.    
+let enter = prompt("Write a number to choose between : \n1) sign up \n2) log in \n3) change the password").toLowerCase() // ila bghiti tkhorji ketbi exit.
 
 function capitalWithoutSpacing(parametre) {
     return parametre.trim().split(" ").map(e => e.charAt(0).toUpperCase() + e.slice(1).toLowerCase()).join(" ")
@@ -14,60 +24,62 @@ while (enter) {
     if (enter == exit) {
         enter = prompt("Write a number to choose between : \n1) sign up \n2) log in \n3) change the password").toLowerCase() // ila bghiti tkhorji ketbi exit.    
     } else if (enter == 1) {
-        // // start name
-        // let fullName = prompt(`to sign up enter youre name`)
-        // if (fullName.length < 5 || fullName.match("  ") || /[^a-zA-Z ]/.test(fullName)) {
-        //     while (fullName.length < 5 || fullName.match("  ") || /[^a-zA-Z ]/.test(fullName)) {
-        //         if (fullName.length < 5) {
-        //             fullName = prompt(`${capitalWithoutSpacing(fullName)} is small, to sign up enter youre full name`)
-        //         } else if (/[^a-zA-Z ]/.test(fullName)) {
-        //             fullName = prompt(`${capitalWithoutSpacing(fullName)} write a new full name witouth symobles or numbers !`)
-        //         }
-        //         console.log(capitalWithoutSpacing(fullName));
-        //     }
-        // }
-        // console.log(capitalWithoutSpacing(fullName));
-        // // end name
+        // start name
+        let fullName = prompt(`to sign up enter youre name`)
+        if (fullName.length < 5 || fullName.match("  ") || /[^a-zA-Z ]/.test(fullName)) {
+            while (fullName.length < 5 || fullName.match("  ") || /[^a-zA-Z ]/.test(fullName)) {
+                if (fullName.length < 5) {
+                    fullName = prompt(`${capitalWithoutSpacing(fullName)} is small, to sign up enter youre full name`)
+                } else if (/[^a-zA-Z ]/.test(fullName)) {
+                    fullName = prompt(`${capitalWithoutSpacing(fullName)} write a new full name witouth symobles or numbers !`)
+                }
+            }
+        }
+        //* console.log(capitalWithoutSpacing(fullName));
+        user.name.push(capitalWithoutSpacing(fullName))
+        // end name
 
-        // // start email
-        // let fullEmail = prompt(`to sign up enter youre Email`)
-        // if (fullEmail.length < 10 || fullEmail.includes(" ") || !fullEmail.includes("@") || !fullEmail.includes(".") || fullEmail.split("@").length !== 2) {
-        //     while (fullEmail.length < 10 || fullEmail.includes(" ") || !fullEmail.includes("@") || !fullEmail.includes(".") || fullEmail.split("@").length !== 2) {
-        //         if (fullEmail.length < 10) {
-        //             fullEmail = prompt(`${lowerWithouthSpacing(fullEmail)} is small, to sign up enter youre full Email`)
-        //         } else if (!fullEmail.includes("@") || !fullEmail.includes(".")) {
-        //             fullEmail = prompt(`${lowerWithouthSpacing(fullEmail)} write a new full name with @ and . !`)
-        //         }else if (fullEmail.includes(" ") || fullEmail.split("@").length !== 2){
-        //             fullEmail = prompt(`${lowerWithouthSpacing(fullEmail)} write a new full name withouth double @ and withouth space !`)
-        //         }
-        //         console.log(lowerWithouthSpacing(fullEmail));
-        //     }
-        // }
-        // console.log(lowerWithouthSpacing(fullEmail));
-        // // end email
+        // start email
+        let fullEmail = prompt(`to sign up enter youre Email`)
+        if (fullEmail.length < 10 || fullEmail.includes(" ") || !fullEmail.includes("@") || !fullEmail.includes(".") || fullEmail.split("@").length !== 2) {
+            while (fullEmail.length < 10 || fullEmail.includes(" ") || !fullEmail.includes("@") || !fullEmail.includes(".") || fullEmail.split("@").length !== 2) {
+                if (fullEmail.length < 10) {
+                    fullEmail = prompt(`${lowerWithouthSpacing(fullEmail)} is small, to sign up enter youre full Email`)
+                } else if (!fullEmail.includes("@") || !fullEmail.includes(".")) {
+                    fullEmail = prompt(`${lowerWithouthSpacing(fullEmail)} write a new full name with @ and . !`)
+                } else if (fullEmail.includes(" ") || fullEmail.split("@").length !== 2) {
+                    fullEmail = prompt(`${lowerWithouthSpacing(fullEmail)} write a new full name withouth double @ and withouth space !`)
+                }
+                //* console.log(lowerWithouthSpacing(fullEmail));
+            }
+        }
+        //* console.log(lowerWithouthSpacing(fullEmail));
+        user.email.push(fullEmail)
+        // end email
 
-        // // start age
-        // let fullAge = prompt(`to sign up enter youre Age`)
-        // if (fullAge.length > 2 || fullAge.match("  ") || /[^0-9 ]/.test(fullAge) || fullAge.includes(" ")) {
-        //     while (fullAge.length > 2 || fullAge.match("  ") || /[^0-9 ]/.test(fullAge) || fullAge.includes(" ")) {
-        //         if (fullAge.match("  ") || /[^0-9 ]/.test(fullAge)) {
-        //             fullAge = prompt(`${fullAge} you write not a number`)
-        //         } else if (fullAge.includes(" ")) {
-        //             fullAge = prompt(`${fullAge} you write a age with a space enter a real Age`)
-        //         } else {
-        //             fullAge = prompt(`${fullAge} you write is big to sign up enter youre Age`)
-        //         }
-        //     }
-        // }
-        // console.log(+fullAge);
-        // // end age
+        // start age
+        let fullAge = prompt(`to sign up enter youre Age`)
+        if (fullAge.length > 2 || fullAge.match("  ") || /[^0-9 ]/.test(fullAge) || fullAge.includes(" ")) {
+            while (fullAge.length > 2 || fullAge.match("  ") || /[^0-9 ]/.test(fullAge) || fullAge.includes(" ")) {
+                if (fullAge.match("  ") || /[^0-9 ]/.test(fullAge)) {
+                    fullAge = prompt(`${fullAge} you write not a number`)
+                } else if (fullAge.includes(" ")) {
+                    fullAge = prompt(`${fullAge} you write a age with a space enter a real Age`)
+                } else {
+                    fullAge = prompt(`${fullAge} you write is big to sign up enter youre Age`)
+                }
+            }
+        }
+        //* console.log(+fullAge);
+        user.age.push(+fullAge)
+        // end age
 
         // start password
         let fullPassword = prompt(`to sign up enter your Password`)
         while (fullPassword.length < 7 || fullPassword.includes(" ") || !/[#@+\-*/0-9]/.test(fullPassword)) {
             fullPassword = prompt(`Password must be at least 7 characters, contain one of [# @ + - * /], and no spaces`)
         }
-        console.log(`the new password : ${fullPassword} is accepted`);
+        alert(`the new password : ${fullPassword} is accepted`)
         // end password
 
         // start confirm password
@@ -75,16 +87,56 @@ while (enter) {
         while (fullConfirmPassword !== fullPassword) {
             fullConfirmPassword = prompt(`the password incorrect`)
         }
-        console.log(`the password : ${fullPassword} is confirmed`);
+        alert(`the password : ${fullPassword} is confirmed`)
+        user.password.push(fullPassword)
         // end confirm password
+
+        let confimData = confirm(`your name is: ${user.name[0]} \nyour email is: ${user.email[0]} \nyour age is: ${user.age[0]} \nyour password is: ${user.password[0]} \nif you want to change the password clique ok`)
+
+        if (confimData == true) {
+            // start new password
+            let checkEmail = prompt(`enter your email to change the password`)
+            if (checkEmail == user.email[0]) {
+                let fullNewPassword = prompt(`enter new Password`)
+                while (fullNewPassword.length < 7 || fullNewPassword.includes(" ") || !/[#@+\-*/0-9]/.test(fullNewPassword)) {
+                    fullNewPassword = prompt(`Password must be at least 7 characters, contain one of [# @ + - * /], and no spaces`)
+                }
+                alert(`the new password : ${fullNewPassword} is changed`)
+                user.password.splice(0, 1, fullNewPassword)
+                alert(`your signing up is successfully \nyour name is: ${user.name[0]} \nyour email is: ${user.email[0]} \nyour age is: ${user.age[0]} \nyour password is: ${user.password[0]}`)
+            } else {
+                alert(`you are not the same user "siir bhalek"!`)
+            }
+            // end new password
+        } else {
+            alert(`your signing up is successfully \nyour name is: ${user.name[0]} \nyour email is: ${user.email[0]} \nyour age is: ${user.age[0]} \nyour password is: ${user.password[0]}`)
+
+        }
+
+
+        while (true) {
+            let enterLogin = prompt(`to log in enter youre email`)
+
+            if (enterLogin == user.email[0]) {
+                let enterLogPassword = prompt(`to log in enter youre password`)
+                if (enterLogPassword == user.password[0]) {
+                    alert(`you successfully log in`)
+                    break
+                }
+            } else {
+                alert("youre not the same user")
+            }
+        }
+
+        console.log(user);
+        
+
         break
     } else if (enter == 2) {
-        prompt(`number is ${enter}`)
+        alert(`you need to sign in first`)
         break
     } else if (enter == 3) {
-        prompt(`number is ${enter}`)
+        alert(`you need to sign in first`)
         break
-    } else {
-        enter = prompt(`${enter} is not found you can write (exit) if you wanna get out or choose a number to choose between : \n1) sign up \n2) log in \n3) change the password`)
     }
 }
